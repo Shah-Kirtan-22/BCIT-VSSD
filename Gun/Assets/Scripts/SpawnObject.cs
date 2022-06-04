39bd6a3c5fa476a2b7c9a1aa256f2487059c0c5c
KIRTAN SHAH <skirtan98@gmail.com>
2022-06-04 12:43:09 -0600

    Game object instantiate
    Instantiate the machine gun game object and store it in a list.

---
+++ Gun/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> objectsToSpawn;  // a list holding the objects to spawn
+    [SerializeField]
+    private List<GameObject> spawnedObjects;  // a list holding the spawned objects
+
+    private int i = 0; // an index referring to the position on the list (should there be more objects: updated in the function)
+
+    private void Awake()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        GameObject gameObject = Instantiate(objectsToSpawn[i]);  // spawn the gameobject (machine gun) with its original position and rotation
+        spawnedObjects.Add(gameObject); // add the spawned gameobject to the spawned list for future use
+    }
+}

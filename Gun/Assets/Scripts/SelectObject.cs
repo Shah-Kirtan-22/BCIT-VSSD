0813b5b69b832bf416768f310d10ee0e050894e9
KIRTAN SHAH <skirtan98@gmail.com>
2022-06-05 12:49:16 -0600

    Mouse button functionality
    Middle mouse button - move the game object
    Right mouse button - rotate the game object
    
    Known bugs:
    Right mouse button - part moves to a random position

--- Gun/Assets/Scripts/SelectObject.cs
+++ Gun/Assets/Scripts/SelectObject.cs
@@ -9,12 +9,39 @@ public class SelectObject : MonoBehaviour
     [SerializeField]
     private Material unSelectMaterial;  // ideally get the material of the selected component from its renderer
 
-    private Transform raycastObject;
-    private Transform selectedObject;
+    private Transform raycastObject;  // highlight this object on hover
+    private Transform selectedObject;  // the selected object on hover
+    private Vector3 mousePosition;  // variable to convert and store the mouse position as a 3D vector
+
+    [SerializeField]
+    private GameObject machineGun;  // reference to the gameobject (Ideally if more objects, use the index from spawnobject script)
+
+    [SerializeField]
+    [Range(0.1f,3.0f)]
+    private float rotationSpeed = 5f;  // speed at which the object can be rotated
+
+    private void Start()
+    {
+        machineGun = GameObject.FindGameObjectWithTag("MachineGun");
+    }
 
     private void Update()
     {
+        // store the current mouse position in the vector3 variable
+
+        mousePosition.x = Input.mousePosition.x;
+        mousePosition.y = Input.mousePosition.y;
+
         CheckRayCast();
+
+        if (Input.GetMouseButton(2))  // middle mouse button held down
+        {
+            ChangePosition();
+        }
+        else if(Input.GetMouseButton(1))   // right mouse button held down
+        {
+            ChangeRotation();
+        }
     }
 
     public void CheckRayCast()
@@ -33,7 +60,12 @@ public class SelectObject : MonoBehaviour
         if (Physics.Raycast(ray, out raycastHit))
         {
             raycastObject = raycastHit.transform;
-
+            
+            if(Input.GetMouseButton(0))  // left mouse button held down
+            {
+                raycastObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 5f));
+            }
+            
             Renderer raycastObjectRenderer = raycastObject.GetComponent<Renderer>();
 
             if (raycastObjectRenderer != null)
@@ -43,5 +75,19 @@ public class SelectObject : MonoBehaviour
 
             selectedObject = raycastObject;
         }
+    }
+
+    // This method is called when the player presses and holds down middle mouse button (scroller)
+    // The method is responsible for changing the position of the gameobject according to the mouse position in world space
+    public void ChangePosition()
+    {
+        machineGun.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10f));
+    }
+    
+    // This method is called when the player presses and holds down right mouse button
+    // The method is responsible for altering the rotation value according to the mouse X and mouse Y
+    public void ChangeRotation()
+    {
+        machineGun.transform.Rotate(new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0) * rotationSpeed * Time.deltaTime * 360);
     }
 }

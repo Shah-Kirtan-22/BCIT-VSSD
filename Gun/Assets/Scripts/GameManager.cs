1e559b8aaad36daea23154b7748e0ace5720db29
KIRTAN SHAH <skirtan98@gmail.com>
2022-06-06 13:19:43 -0600

    XRay mode
    Apply a wireframe shader on the material

--- Gun/Assets/Scripts/GameManager.cs
+++ Gun/Assets/Scripts/GameManager.cs
@@ -2,27 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     SpawnObject spawnObject;
+    [SerializeField]
+    Slider zoomSlider;
+    public Material wireFrameMaterial;
 
     private void Start()
     {
         spawnObject = GameObject.Find("SpawnLocation").GetComponent<SpawnObject>();
     }
 
-
     public void ResetGame()
     {
         Time.timeScale = 0;
         RestartGame();
-        /*
-        Destroy(spawnObject.spawnedObjects[0]);  // destroy the gameobject in the scene (replace with the index number if more than one object)
-        spawnObject.spawnedObjects.Clear();  // clear the list first
-
-        spawnObject.Spawn();
-        */
     }
 
     public void RestartGame()
@@ -30,4 +27,16 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void WireFrame()
+    {
+        GL.wireframe = true;
+        Renderer[] tempRenderer = GameObject.FindObjectsOfType<Renderer>();
+        foreach(Renderer r in tempRenderer)
+        {
+            r.material = wireFrameMaterial;
+            
+        }
+    }
+
 }
